deb2933e443b02ab6b589307dbf5b6a1102a2d84
Tiago Siqueira <tvsjazz@gmail.com>
2021-03-31 11:15:23 -0300

    post book

--- src/main/java/com/tvs/bibliotecaapi/resources/BookResource.java
+++ src/main/java/com/tvs/bibliotecaapi/resources/BookResource.java
@@ -1,10 +1,12 @@
 package com.tvs.bibliotecaapi.resources;
 
+import com.tvs.bibliotecaapi.dto.MessageResponseDTO;
 import com.tvs.bibliotecaapi.exception.BookNotFoundException;
 import com.tvs.bibliotecaapi.models.Book;
 import com.tvs.bibliotecaapi.service.BookService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +28,10 @@ public class BookResource {
         return bookService.findById(id);
     }
 
-    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createBook(@RequestBody Book book) {
+        return bookService.createPerson(book);
+    }
 
 }
--- src/main/java/com/tvs/bibliotecaapi/service/BookService.java
+++ src/main/java/com/tvs/bibliotecaapi/service/BookService.java
@@ -1,6 +1,7 @@
 package com.tvs.bibliotecaapi.service;
 
 import com.tvs.bibliotecaapi.dto.BookDTO;
+import com.tvs.bibliotecaapi.dto.MessageResponseDTO;
 import com.tvs.bibliotecaapi.exception.BookNotFoundException;
 import com.tvs.bibliotecaapi.models.Book;
 import com.tvs.bibliotecaapi.repository.BookRepository;
@@ -20,6 +21,18 @@ public class BookService {
 
     private BookRepository bookRepository;
 
+    private Book verifyIfExists(Long id) throws BookNotFoundException {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException(id));
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
+    }
+
     public List<Book> listAll(int pagina, int qtd) {
         Pageable paginacao = PageRequest.of(pagina, qtd);
         Page<Book> allBooks = bookRepository.findAll(paginacao);
@@ -31,8 +44,11 @@ public class BookService {
         return book;
     }
 
-    private Book verifyIfExists(Long id) throws BookNotFoundException {
-        return bookRepository.findById(id)
-                .orElseThrow(() -> new BookNotFoundException(id));
+    public MessageResponseDTO createPerson(Book book) {
+        Book bookSaved = bookRepository.save(book);
+        return createMessageResponse(bookSaved.getId(), "Created book with ID ");
     }
+
+
+
 }
